765066a062cfd57f3f05cc1a229d9574c905943d
Gcedro <gcedro0408@outlook.com>
2026-04-14 11:57:31 -0300

    exercicio 3

---
+++ exercicio4.py
@@ -0,0 +1,21 @@
+
+nota1 = 8
+nota2 = 6
+nota3 = 8
+nota4 = 7
+
+med_final = (nota1 + nota2 + nota3 + nota4) / 4
+
+if med_final >= 7:
+   print ("Aprovado")
+
+elif med_final >= 5:
+     print("Recuperação")
+
+else: 
+   print("Reprovado")
+
+
+
+
+
---
+++ exercicio5.py
@@ -0,0 +1,8 @@
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+
+if A % B == 0 or B % A == 0:
+    print("São Múltiplos")
+
+else:
+    print("Não Múltiplos")
